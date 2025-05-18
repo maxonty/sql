0d0f9ad4b0d6ed5caf486a00806d5a449d01fe38
maxonty <162309431+maxonty@users.noreply.github.com>
2025-05-18 16:38:34 +0300

    delete_queries.sql

--- delete_queries.sql
+++ delete_queries.sql
@@ -1,2 +1,20 @@
--- Удалить бронь
-DELETE FROM booking WHERE id = 1;
+-- Удалить заказ услуги
+DELETE FROM service_order WHERE id = 1;
+
+-- Удалить клиента (если нет внешних зависимостей)
+DELETE FROM client WHERE id = 3;
+
+-- Удалить номер (если не участвует в бронированиях)
+DELETE FROM room WHERE id = 5;
+
+-- Удалить бронирование
+DELETE FROM booking WHERE id = 2;
+
+-- Удалить отель
+DELETE FROM hotel WHERE id = 3;
+
+-- Удалить сотрудника
+DELETE FROM employee WHERE id = 2;
+
+-- Удалить услугу
+DELETE FROM service WHERE id = 3;
